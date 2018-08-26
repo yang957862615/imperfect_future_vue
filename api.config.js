61a146df744d11de3f250d36a840ca4e934cd82e
yang957862615 <957862615@qq.com>
2018-08-26 15:10:55 +0800

    update api url

--- api.config.js
+++ api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'prod');
 
 module.exports = {
-  baseURL: isProdMode ? 'https://rest.joegreens.cn/' : 'http://192.168.199.207:9999'
+  baseURL: 'https://localhost:9999/'
 };
