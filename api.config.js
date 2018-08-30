a800cefe685bdffc861d8d9f85b4d079707cf5bb
yang957862615 <957862615@qq.com>
2018-08-30 22:22:56 +0800

    update api url 5

--- api.config.js
+++ api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production');
 
 module.exports = {
-  baseURL: isProdMode ? 'https://localhost:9999/' : 'http://127.0.0.1:9999/'
+  baseURL: isProdMode ? 'http://localhost:9999/' : 'http://127.0.0.1:9999/'
 };
